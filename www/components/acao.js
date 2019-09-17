3ffb6df4811be56592bde6f197fb3400b28d777e
ThiagoFranco25 <thiago.ferreira25102002@gmail.com>
2019-09-18 02:47:28 +0900

    atualizações

--- www/components/acao.js
+++ www/components/acao.js
@@ -16,3 +16,29 @@ $(document).on("click", "#camera", function(){
     navigator.notification.alert("Failed because: " + message);
   }
 });
+$(document).on("change", "#opacidade", function(){
+  var vlo = window.document.getElementById('opacidade');
+  var vl1 = Number(vlo.value);
+var a = window.document.getElementById('image'); 
+  a.style.filter = `opacity(${vl1}%)`; 
+});
+$(document).on("change", "#saturacao", function(){
+  var valor = window.document.getElementById('saturacao');
+  var vl2 = Number(valor.value);
+var b = window.document.getElementById('image'); 
+  b.style.filter = `saturate(${vl2}%)`;
+
+});
+$(document).on("change", "#escala", function(){
+  var vlr = window.document.getElementById('escala');
+  var vl3 = Number(vlr.value);
+var c = window.document.getElementById('image'); 
+  c.style.filter = `grayscale(${vl3}%)`;
+
+});
+$(document).on("change", "#desfoque", function(){
+  var a = window.document.getElementById('desfoque');
+  var vl4 = Number(a.value);
+var d = window.document.getElementById('image'); 
+  d.style.filter = `blur(${vl4}px)`;
+});
